921a269b14e8e1ca56a20c1a9d616917398867e3
Kirk Easterson <kirk.easterson@gmail.com>
2025-05-01 13:23:37 +0200

    feat(firefox): add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "${HOME}/.mozilla/firefox" ]; then
+  echo "Firefox directory does not exist. Exiting..."
+  exit 1
+fi
+
+FIREFOX_DIR=$(find "${HOME}/.mozilla/firefox/" -type d -regex ".*default-release$")
+
+if [ -z "$FIREFOX_DIR" ]; then
+  echo "Firefox profile directory doesn't exist. Exiting..."
+  exit 1
+fi
+
+echo "Installing firefox config to ${FIREFOX_DIR}"
+stow -S firefox --target="${FIREFOX_DIR}" --adopt
+
+# cd - >/dev/null || exit
